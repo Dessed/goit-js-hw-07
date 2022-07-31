3798a89128cf5b7765bbdc51ff6f314aed63212b
Dessed <des.ztrcnto@gmail.com>
2022-07-31 20:27:47 +0300

    mod

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
-
-
 const gallery = document.querySelector('.gallery');
 const fotoMarkup = creatingImageGallery(galleryItems);
 
@@ -12,7 +10,7 @@ function creatingImageGallery(pictures) {
     return galleryItems.map(({preview, original, description}) => {
         return `
         <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
+        <a class="gallery__link" href="${original}" >
           <img
             class="gallery__image"
             src="${preview}"
@@ -24,20 +22,21 @@ function creatingImageGallery(pictures) {
     }).join('');
 };
 
-// const basicLightbox = require('basiclightbox') 
-// import * as basicLightbox from 'basiclightbox';
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `)
-
-// instance.show()
-
-console.log(galleryItems);
 
 
+  gallery.addEventListener('click', (evt) => {
+    evt.preventDefault();
+console.log(evt.target.classList);
+    let pic = evt.target.src;
+    // console.log(evt.target.href);
+    // console.log(evt.target.classList.contains('gallery__link'));
+      basicLightbox.create(`
+      <img width="1400" height="900" src="${pic}">
+    `).show()
+  });
+
+const pic = galleryItems.map(({preview, original, description}) => {
+  return original;
+})
+
+console.log(pic);
