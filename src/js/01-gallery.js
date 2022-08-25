51e2c00632fb7a8f138491e939d3af65b5bb77b2
Dessed <des.ztrcnto@gmail.com>
2022-08-25 21:16:33 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 
-// Change code below this line
 const gallery = document.querySelector('.gallery');
 
 const fotoMarkup = creatingImageGallery(galleryItems);
-// console.log(fotoMarkup);
 
 gallery.insertAdjacentHTML('beforeend', fotoMarkup);
 
@@ -26,43 +24,30 @@ function creatingImageGallery(pictures) {
 
 
 gallery.addEventListener('click', onGalleryBasicLightbox)
-    
-
 
 function onGalleryBasicLightbox (evt) {
   evt.preventDefault();
 
   let originalPicture = evt.target.dataset.source;
 
-//   basicLightbox.create(`
-//   <img width="1400" height="900" src="${originalPicture}">
-// `).show()
-
   const html = `
   <img width="1400" height="900" src="${originalPicture}">
 	`
-
 	basicLightbox.create(html, {
-		onClose: document.addEventListener("keydown", function (evt) {
+		// onClose: () => {alert ('Вы уверены?')},
+    onShow: (instance) => { 
+      
+    document.addEventListener("keydown", closeLightboxButton)
+
+    function closeLightboxButton(evt) {
       if (evt.key === `Escape`) {
-        console.log("key: ", evt.key);
-        return true;
-        
-      }
-      return false;
-    })
+        instance.close();
+      } 
+    }},
 	}).show()
+
+ 
 }
 
 
-// document.addEventListener("keydown", closeLightboxButton)
-
-// function closeLightboxButton(evt) {
-//   if (evt.key === `Escape`) {
-//     console.log("key: ", evt.key);
-//     return true;
-    
-//   }
-//   return false;
-// }
 
